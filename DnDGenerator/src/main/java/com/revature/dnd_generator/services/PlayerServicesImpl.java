cff450340e2c853ef303486945568a446bc2d41a
Christopher Raleigh <craleigh318@comcast.net>
2019-02-13 20:29:09 -0500

    Created PlayerFactory.

--- DnDGenerator/src/main/java/com/revature/dnd_generator/services/PlayerServicesImpl.java
+++ DnDGenerator/src/main/java/com/revature/dnd_generator/services/PlayerServicesImpl.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import com.revature.dnd_generator.data.PlayerDao;
 import com.revature.dnd_generator.exceptions.IncorrectLoginException;
 import com.revature.dnd_generator.model.Player;
+import com.revature.dnd_generator.model.PlayerFactory;
 public class PlayerServicesImpl implements PlayerServices {
 
 	private static final Logger LOGGER = LogManager.getLogger(PlayerServicesImpl.class);
@@ -14,7 +15,7 @@ public class PlayerServicesImpl implements PlayerServices {
 	public Player attemptLogin(String username, String password) {
 		try {
 			int userId = PlayerDao.getInstance().logIn(username, password);
-			return new Player(username, userId);
+			return PlayerFactory.createNoPassword(userId, username);
 		} catch(IncorrectLoginException e ){
 			LOGGER.error(e.getMessage(), e);
 			return new Player();
