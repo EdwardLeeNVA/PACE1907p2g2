8b000ebe1406e9037a0918e0171a05867ede2763
Edward Lee <edwardleenva@verizon.net>
2019-02-20 17:18:06 -0500

    Switched to using the submitted player_id

--- DnDGenerator/src/main/java/com/revature/dnd_generator/dispatcher/CharacterDelegate.java
+++ DnDGenerator/src/main/java/com/revature/dnd_generator/dispatcher/CharacterDelegate.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dnd_generator.model.Player;
 import com.revature.dnd_generator.services.CharacterServices;
 import com.revature.dnd_generator.services.CharacterServicesImpl;
 import com.revature.dnd_generator.services.PlayerServices;
@@ -27,9 +28,10 @@ public class CharacterDelegate implements BaseDelegate{
 		String[] path = req.getRequestURI().split("/");
 		//get all from current user
 		if(path.length == 4) {
-			LOGGER.info("Fetching all with userid: " +(int)req.getSession().getAttribute("playerID"));
+            Player input = mapper.readValue(req.getReader(), Player.class);			
+            LOGGER.info("Fetching all with userid: " +((Integer)req.getSession().getAttribute("playerID")).intValue());
 			res.setContentType("application/json");
-			String jsonInString = mapper.writeValueAsString(cService.getAllPlayerCharacters(1));
+			String jsonInString = mapper.writeValueAsString(cService.getAllPlayerCharacters(input.getId()));
 			LOGGER.info("LOGIN OBJECT RETURNED: as JSON STRING:  "+jsonInString);
 			res.getWriter().write(jsonInString);
 		}else if(path.length == 5) {//get the requested object
