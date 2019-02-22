e2dba15faa938f4fc7271b6f47691fedc13b681d
Edward Lee <edwardleenva@verizon.net>
2019-02-21 20:18:00 -0500

    Updated services and dispatcher to return the character Id (This breaks the code)

--- DnDGenerator/src/main/java/com/revature/dnd_generator/services/CharacterServices.java
+++ DnDGenerator/src/main/java/com/revature/dnd_generator/services/CharacterServices.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.revature.dnd_generator.model.DndCharacter;
 
 public interface CharacterServices {
-	public boolean saveDndCharacter(DndCharacter d);
+	public int saveDndCharacter(DndCharacter d);
 	public DndCharacter getDndCharacter(int characterId);
 	public List<DndCharacter> getAllPlayerCharacters(int playerId);
 	public boolean deleteCharacter(DndCharacter d);
